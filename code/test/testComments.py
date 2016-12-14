b61a959f39afd8b094588decb37f08ff044672f4
Danixk <dadric9@gmail.com>
2016-12-14 14:26:28 +0000

    test

--- code/test/testComments.py
+++ code/test/testComments.py
@@ -13,6 +13,9 @@ sys.path.append('../')
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"lowlevel")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
+
+
+sys.path.insert(0, '../lowlevel')
 # ------------------------------ #
 
 import messages
